e9ea9478ad929d812155306e6e311d46f84cad89
nickhsuu <nickhsu723@gmail.com>
2022-05-13 15:44:27 -0400

    Gatsby Tutorial Pt 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from './layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>I'm Nick. I like solving problems.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { title } from './main.module.css'
+
+const Main = () => {
+    return(
+        <h1> className = {title}>
+            The Title Page you didn't know you wanted.
+        </h1>
+    )
+}
+
+export default Main
